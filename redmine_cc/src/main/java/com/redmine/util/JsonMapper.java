45e2f671d4883da4729c26e16e81b572fc02699e
陈成 <chen.cheng214@chinaredstar.com>
2017-02-28 15:20:02 +0800

    add README

---
+++ redmine_cc/src/main/java/com/redmine/util/JsonMapper.java
@@ -0,0 +1,80 @@
+package com.redmine.util;
+
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class JsonMapper {
+
+    private static ObjectMapper m = new ObjectMapper();
+    private static JsonFactory jf = new JsonFactory();
+
+    public static <T> Object fromJson(String jsonAsString, Class<T> pojoClass) {
+        Object object = null;
+        try {
+            object = m.readValue(jsonAsString, pojoClass);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return object;
+    }
+
+    public static <T> Object fromJson(FileReader fr, Class<T> pojoClass) {
+        Object object = null;
+        try {
+            object = m.readValue(fr, pojoClass);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return object;
+    }
+
+    public static String toJson(Object pojo, boolean prettyPrint) {
+        StringWriter sw = new StringWriter();
+        JsonGenerator jg = null;
+        try {
+            jg = jf.createJsonGenerator(sw);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (prettyPrint) {
+            jg.useDefaultPrettyPrinter();
+        }
+        try {
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            m.getSerializationConfig().setDateFormat(df);
+            m.writeValue(jg, pojo);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sw.toString();
+    }
+
+    public static void toJson(Object pojo, FileWriter fw, boolean prettyPrint) {
+        JsonGenerator jg = null;
+        try {
+            jg = jf.createJsonGenerator(fw);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (prettyPrint) {
+            jg.useDefaultPrettyPrinter();
+        }
+        try {
+            m.writeValue(jg, pojo);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ redmine_cc/web/WEB-INF/jsp/redmine.jsp
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ include file="/WEB-INF/tags.jsp" %>
+<html>
+<head>
+    <title>Adam</title>
+</head>
+<body>
+<div class="container-fluid">
+    //////
+</div>
+</body>
+</html>
